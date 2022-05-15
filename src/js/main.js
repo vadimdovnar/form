cda015942eb84e5ee49e49615a74a651dcb6b5dd
vadimdovnar <84572478+vadimdovnar@users.noreply.github.com>
2022-05-16 01:29:37 +0300

    create correct validation

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,16 @@
 (function ($) {
     "use strict";
 
+    var inputPhone = $('.input-phone');
+    inputPhone.on('click', function() {
+        $(this).val('+375');
+    })
+
+    inputPhone.on('blur', function() {
+        if($(this.value == '+375')) {
+            $(this).val(null);
+        }
+    })
 
      /*==================================================================
     [ Focus input ]*/
@@ -42,15 +52,56 @@
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
+
+        var rule = $(input).attr('data-rule');
+        var result = $(input).val().trim() == '' ? false : validDataRule(rule, input);
+        
+        return result;
+        
+
+        // if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        //     if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        //         return false;
+        //     }
+        // }
+        // else {
+        //     if($(input).val().trim() == ''){
+        //         return false;
+        //     }
+        // }
+    }
+
+    function validDataRule(rule, input) {
+        var length = +$(input).val().length;
+        var value = $(input).val();
+        var lengthFrom = +$(input).attr('data-from');
+        var lengthTo = +$(input).attr('data-to');
+
+        switch(rule) {
+            
+            case 'string-company':
+                if( ((value.trim().match(/[a-z]+|[0-9]+/i) == null) || ((length < lengthFrom) || (length > lengthTo)))) {
+                    return false;
+                }
+            break;
+                
+            case 'string-name':
+                if( ((value.trim().match(/^[A-Z]+$|^[А-Я]+$/i) == null) || ((length < lengthFrom) || (length > lengthTo)))) {
+                    return false;
+                }
+            break;
+                
+            case 'email': 
+                if(value.trim().match(/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i) == null) {
+                    return false;
+                }
+            break;
+                
+            case 'phone':
+                if(value.trim().match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/) == null) {
+                    return false;
+                }
+            break;
         }
     }
 
@@ -66,7 +117,6 @@
         $(thisAlert).removeClass('alert-validate');
     }
 
-    const valid = new Validation();
-    valid.validateForm();
+    
 
 })(jQuery);
