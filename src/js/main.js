a3a99f0e2c0a5b3b831c9f9b9871fc932817bb4d
vadimdovnar <84572478+vadimdovnar@users.noreply.github.com>
2022-05-16 11:34:00 +0300

    add correct validation for Company, FirstName, LastName, Email, Phone filds

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@
     })
 
     inputPhone.on('blur', function() {
-        if($(this.value == '+375')) {
+        if($(this).val() == '+375') {
             $(this).val(null);
         }
     })
@@ -57,18 +57,6 @@
         var result = $(input).val().trim() == '' ? false : validDataRule(rule, input);
         
         return result;
-        
-
-        // if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        //     if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-        //         return false;
-        //     }
-        // }
-        // else {
-        //     if($(input).val().trim() == ''){
-        //         return false;
-        //     }
-        // }
     }
 
     function validDataRule(rule, input) {
